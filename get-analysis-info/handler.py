0a16cdc0ceb4de4ced670a1caa3120ebfd26161e
John Wregglesworth <wregglej@iplantcollaborative.org>
2019-10-22 08:16:01 -0700

    Use de-graphql in get-analysis-info insted of hitting the apps service directly

--- get-analysis-info/handler.py
+++ get-analysis-info/handler.py
@@ -9,23 +9,118 @@ import requests
 import requests.exceptions as HTTPError
 
 def handle(req):
-    apps_base_url = os.environ["APPS_URL"]
-
-    apps_url = urlparse(apps_base_url)
-    apps_url = apps_url._replace(path="/analyses")
-    apps_url_str = urlunparse(apps_url)
+    graphql_url = os.environ["GRAPHQL_URL"]
 
     parsed_req = json.loads(req)
     analysis_to_check = parsed_req["id"]
     user = parsed_req["user"]
 
-    response  = requests.get(
-        apps_url_str,
-        params={
-            'user' : user,
-            'filter' : json.dumps([{'field' : 'id', 'value' : analysis_to_check}])
+    response = requests.post(
+        graphql_url,
+        json={
+            'query' : """
+                query GetAnalysisInfo($user: String, $id: String) {
+                    analysis(username: $user, analysisID: $id) {
+                        id
+                        name
+                        description
+                        can_share
+                        batch_status {
+                            total
+                            completed
+                            running
+                            submitted
+                        }
+                        batch
+                        startdate
+                        enddate
+                        status
+                        parent_id
+                        interactive_urls
+                        wiki_url
+                        notify
+                        resultfolderid
+                        app {
+                            id
+                            app_type
+                            beta
+                            can_favor
+                            can_rate
+                            can_run
+                            debug
+                            deleted
+                            description
+                            disabled
+                            edited_date
+                            groups
+                            integration_date
+                            integrator_email
+                            integrator_name
+                            is_favorite
+                            is_public
+                            label
+                            name
+                            permission
+                            pipeline_eligibility {
+                                is_valid
+                                reason
+                            }
+                            rating {
+                                average
+                                total
+                                user
+                                comment_id
+                            }
+                            requirements {
+                                min_cpu_cores
+                                min_memory_limit
+                                min_disk_space
+                                max_cpu_cores
+                                memory_limit
+                                step_number
+                            }
+                            step_count
+                            system_id
+                            wiki_url
+                        }
+                        steps {
+                            step_number
+                            external_id
+                            enddate
+                            startdate
+                            status
+                            updates {
+                                status
+                                message
+                                timestamp
+                            }
+                            app_step_number
+                            step_type
+                        }
+                        parameters {
+                            data_format
+                            param_type
+                            param_id
+                            info_type
+                            is_default_value
+                            param_name
+                            parameter_value {
+                                value
+                            }
+                            is_visible
+                            full_param_id
+                            data_format
+                        }
+                    }
+                }
+            """,
+            'variables' : {
+                "user" : user,
+                "id" : analysis_to_check
+            }
         }
     )
+
     response.raise_for_status()
 
     logging.warning("response: %s" % response.text)
@@ -34,9 +129,7 @@ def handle(req):
 
     retval = {}
 
-    if len(parsed_response["analyses"]) < 1:
-        abort(404)
-    else:
-        retval = parsed_response["analyses"][0] # looking up analysis by id, so there should only be one.
+    if parsed_response["data"]["analysis"] != None:
+        retval = parsed_response["data"]["analysis"]
 
     return json.dumps(retval)
